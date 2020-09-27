ce8138ce021da2f1548a5d7ecedfa7bb09b419fd
venkey <venkeyvenkatesh885@gmail.com>
2020-09-27 18:15:43 +0530

    first commit

---
+++ Employee_assignment/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Employee_assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int IS_FULL_TIME = 1;
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if (empCheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+
+           
+
+
+        }
+    }
+}
